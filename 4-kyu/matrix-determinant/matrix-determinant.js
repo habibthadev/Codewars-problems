930a9abf3d8842bf0fbdb9c6a08dd1355b9d8101
Habib <adebayohabib7@gmail.com>
2025-03-04 04:07:26 -0800

    Add solution - CodeHub

---
+++ 4-kyu/matrix-determinant/matrix-determinant.js
@@ -0,0 +1,20 @@
+function determinant(matrix) {
+    const n = matrix.length;
+    
+    if (n === 1) {
+        return matrix[0][0];
+    }
+    
+    if (n === 2) {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+    
+    let det = 0;
+    for (let col = 0; col < n; col++) {
+        let minor = matrix.slice(1).map(row => row.filter((_, j) => j !== col));
+        
+        det += ((col % 2 === 0 ? 1 : -1) * matrix[0][col] * determinant(minor));
+    }
+    
+    return det;
+}
