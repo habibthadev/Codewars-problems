1984898a61a6816497bebced3036c59a394d15c7
Habib <adebayohabib7@gmail.com>
2025-03-22 12:10:16 +0100

    Add solution - CodeHub

---
+++ 6-kyu/two-sum/two-sum.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    const numToIndex = new Map();
+​
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num;
+​
+        if (numToIndex.has(complement)) {
+            return [numToIndex.get(complement), i];
+        }
+        numToIndex.set(num, i);
+    }
+  
+    return [];
+}
+​
